340cfbcfbaba5a9b35059be75b807bdd0855dac2
BarHanSolo <andrzej.lippa@gmail.com>
2020-03-23 01:32:02 +0200

    beta
    -firebase support added
    -previous problems fixed
    
    todo:
    -think about photos of product
    -rethink the database
    -reconsider changing database

--- app/src/main/java/com/sortingrocks/sorter/MainActivity.kt
+++ app/src/main/java/com/sortingrocks/sorter/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.sortingrocks.sorter
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.vision.barcode.Barcode
 
@@ -15,6 +20,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+        }
+
         barcodeResult = findViewById(R.id.barcode_result)
     }
     fun scanBarcode(v: View) {
--- app/src/main/java/com/sortingrocks/sorter/Result.kt
+++ app/src/main/java/com/sortingrocks/sorter/Result.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.TextView
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.vision.barcode.Barcode
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Result : AppCompatActivity() {
     lateinit var barcodeResult : TextView
@@ -17,6 +18,22 @@ class Result : AppCompatActivity() {
         barcodeResult = findViewById(R.id.barcodeText)
         val barcode = intent.getStringExtra("barcode")
         barcodeResult.text = barcode
+        var productName = findViewById<TextView>(R.id.nameText)
+
+        //getting data from database
+        var db = FirebaseFirestore.getInstance();
+        val docRef = db.collection("Products")
+        val query = docRef.whereEqualTo("barcode", barcode).get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    var data = document.data
+                    productName.text = document.data.get("name").toString()
+                    //Log.d("TAG", data.get("name").toString())
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents: ", exception)
+            }
     }
 
     fun goBack(v : View){
