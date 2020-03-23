2a8336cf615dc3c90a271a2cc2692302e1f08e42
BarHanSolo <andrzej.lippa@gmail.com>
2020-03-23 19:28:37 +0200

    beta
    -firebase support added in pictures too
    
    todo:
    -maybe tool to upload it all easier
    -ask what others think
    -rethink the database ?
    -reconsider changing database ?

--- app/src/main/java/com/sortingrocks/sorter/Result.kt
+++ app/src/main/java/com/sortingrocks/sorter/Result.kt
@@ -1,17 +1,28 @@
 package com.sortingrocks.sorter
 
+import android.app.ActionBar
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.view.updateLayoutParams
+import com.bumptech.glide.Glide
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.firebase.firestore.FirebaseFirestore
 
 class Result : AppCompatActivity() {
     lateinit var barcodeResult : TextView
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
@@ -19,6 +30,7 @@ class Result : AppCompatActivity() {
         val barcode = intent.getStringExtra("barcode")
         barcodeResult.text = barcode
         var productName = findViewById<TextView>(R.id.nameText)
+        var resultLayout = findViewById<LinearLayout>(R.id.resultLayout)
 
         //getting data from database
         var db = FirebaseFirestore.getInstance();
@@ -28,7 +40,29 @@ class Result : AppCompatActivity() {
                 for (document in documents) {
                     var data = document.data
                     productName.text = document.data.get("name").toString()
-                    //Log.d("TAG", data.get("name").toString())
+
+                    //for each element adding to layout bar, description, type, (photo)
+                    var divider = View(this)
+                    var params = RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    divider.layoutParams = params
+                    divider.updateLayoutParams {
+                        height = 1
+                    }
+                    divider.setBackgroundColor(resources.getColor(R.color.colorDivider))
+                    resultLayout.addView(divider)
+                    var description = TextView(this)
+                    description.text = document.data.get("description").toString()
+                    resultLayout.addView(description)
+                    var type = TextView(this)
+                    type.text = document.data.get("type").toString()
+                    resultLayout.addView(type)
+                    var picture = ImageView(this)
+                    resultLayout.addView(picture)
+                    var picUrl = document.data.get("address")
+                    Glide.with(getApplicationContext()).load(picUrl).into(picture)
                 }
             }
             .addOnFailureListener { exception ->
